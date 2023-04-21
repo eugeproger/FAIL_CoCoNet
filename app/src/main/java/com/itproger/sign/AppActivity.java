12386e6cfc39cbdaca1474ecde116e1de4d11562
Euge <euge.itech@gmail.com>
2023-04-21 11:42:27 +0200

    start create login activity

--- app/src/main/java/com/itproger/sign/AppActivity.java
+++ app/src/main/java/com/itproger/sign/AppActivity.java
@@ -4,21 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 import com.itproger.sign.tabs.adapter.TabsAccessorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class AppActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private TabsAccessorAdapter tabsAccessorAdapter;
+    private FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_app);
 
         toolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(toolbar);
@@ -30,5 +33,18 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.main_tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (currentUser == null) {
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(AppActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
